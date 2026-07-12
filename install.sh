b1fb6afa804d9e02842a3cc3fe39281dc2e1b79b
Quasic <qbasic010@aim.com>
2026-07-12 14:12:42 -0400

    try adding message to install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 cp termux-url-opener ~/bin
 if cd ~/bin
 then
-chmod +x termux-url-opener && echo termux-url-opener installed
+chmod +x termux-url-opener && echo "termux-url-opener installed at $(realpath termux-url-opener)"
 ln -sfb termux-url-opener termux-file-editor && echo termux-file-editor installed as symlink with -b option
 else echo Could not cd to ~/bin
 fi
