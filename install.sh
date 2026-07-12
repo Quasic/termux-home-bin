644bcb6a90d8ad7a413343d41fc3df9a3eb9778e
Quasic <qbasic010@aim.com>
2026-07-12 15:08:50 -0400

    Actions workflow (#8)
    also use pkg instead of apt and tell ~/bin install location realpath

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
-apt update
-apt install dialog
+pkg install dialog
 mkdir -p ~/bin
 if [ -e ~/bin/termux-url-opener ]
 then mv ~/bin/termux-url-opener ~/bin/termux-url-opener~ && echo backed up ~/bin/termux-url-opener to ~/bin/termux-url-opener~
@@ -8,7 +7,7 @@ fi
 cp termux-url-opener ~/bin
 if cd ~/bin
 then
-chmod +x termux-url-opener && echo termux-url-opener installed
+chmod +x termux-url-opener && echo "termux-url-opener installed at $(realpath termux-url-opener)"
 ln -sfb termux-url-opener termux-file-editor && echo termux-file-editor installed as symlink with -b option
 else echo Could not cd to ~/bin
 fi
