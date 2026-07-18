20d9fb48b5ce19e69212fcf94f87f2957bef4141
Quasic <qbasic010@aim.com>
2026-07-17 23:02:04 -0400

    duckduckgo.sh: exec, simplify a bit, also escape &, printf instead of echo

--- duckduckgo.sh
+++ duckduckgo.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/sh
 if [ $# -eq 0 ]
 then
-	echo Enter duckduckgo search:
+	printf 'Enter duckduckgo search:\n'
 	read -r S
 else
-	S=
+	S="$*"
 fi
-/data/data/com.termux/files/home/bin/termux-url-opener "https://duckduckgo.com/?q=$(echo "${S:-$*}"|sed 's/ /+/g')"
+exec /data/data/com.termux/files/home/bin/termux-url-opener "https://duckduckgo.com/?q=$(printf %s "$S"| sed 's/ /+/g; s/&/%26/g')"
