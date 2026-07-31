8104341ec83a7102506408df54e2c095c42e222f
Quasic <qbasic010@aim.com>
2026-07-30 23:24:37 -0400

    test.sh

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+x=0
+fail(){
+printf %s\\n "$1 failed test: $2"
+((x++))
+}
+# check Makefile
+if command -v make
+then
+	make -n all>/dev/null||fail Makefile 'make -n all'
+	make -n proj>/dev/null||fail Makefile 'make -n proj'  # deprecated, due to name conflict, but still testing
+else printf 'SKIPPED TESTS: no make\n'
+fi
+# check shell scripts
+for s in ./*.sh
+do bash -n "$s"||fail "$s" 'bash -n'
+done
+if command -v shellcheck
+then shellcheck ./*.sh||fail '*.sh' shellcheck
+else printf 'SKIPPED TESTS: no shellcheck\n'
+fi
+# check prj.sh
+bash prj.sh version||fail prj.sh version
+# Results
+if [ "$x" = 0 ]
+then printf Passed!\\n
+else
+	printf '%i failures\n' "$x"
+	exit 1
+fi
