235a7b79436f4a7f9b615936ce4c048a21747ad5
Quasic <qbasic010@aim.com>
2026-08-22 20:32:53 -0400

    Update install.sh to include dialog-wrapper.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@ mkdir -p ~/bin
 if [ -e ~/bin/termux-url-opener ]
 then mv ~/bin/termux-url-opener ~/bin/termux-url-opener~ && echo backed up ~/bin/termux-url-opener to ~/bin/termux-url-opener~
 fi
+if [ -e ~/bin/dialog-wrapper.sh ]
+then mv ~/bin/dialog-wrapper.sh ~/bin/dialog-wrapper.sh~ && echo backed up ~/bin/dialog-wrapper.sh to ~/bin/dialog-wrapper.sh~
+fi
 cp termux-url-opener ~/bin
+cp dialog-wrapper.sh ~/bin
 if cd ~/bin
 then
-chmod +x termux-url-opener && echo "termux-url-opener installed at $(realpath termux-url-opener)"
+chmod +x termux-url-opener dialog-wrapper.sh && echo "termux-url-opener and dialog-wrapper.sh installed"
 ln -sfb termux-url-opener termux-file-editor && echo termux-file-editor installed as symlink with -b option
 else echo Could not cd to ~/bin
 fi
